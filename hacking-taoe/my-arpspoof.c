708ff1cb736bbc1397faa17a529695e6fb685417
Pedro Mauro <pedro@debian.bunsen.pedro>
2017-05-12 00:21:44 -0300

    update

--- hacking-taoe/my-arpspoof.c
+++ hacking-taoe/my-arpspoof.c
@@ -2,3 +2,9 @@
 #include <sys/socket.h> 
 #include <linux/if_packet.h> 
 #include <net/ethernet.h>
+
+int main(int argc, char *argv[]) {
+  int packet_socket;
+  
+  packet_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ARP));
+}
