e19069fdc29b5a2ddc24d5a09a18b5b34a17bd6c
Pedro Mauro <pedro@debian.bunsen.pedro>
2017-05-20 02:32:10 -0300

    update

--- hacking-taoe/my-arpspoof.c
+++ hacking-taoe/my-arpspoof.c
@@ -6,8 +6,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+// #include <linux/if_arp.h> // for struct arphdr
 
 #include <errno.h>
+
+struct my_arphdr {
+	__be16		ar_hrd;		/* format of hardware address	*/
+	__be16		ar_pro;		/* format of protocol address	*/
+	unsigned char	ar_hln;		/* length of hardware address	*/
+	unsigned char	ar_pln;		/* length of protocol address	*/
+	__be16		ar_op;		/* ARP opcode (command)		*/
+	 /*
+	  *	 Ethernet looks like this : This bit is variable sized however...
+	  */
+	unsigned char		ar_sha[ETH_ALEN];	/* sender hardware address	*/
+	unsigned char		ar_sip[4];		/* sender IP address		*/
+	unsigned char		ar_tha[ETH_ALEN];	/* target hardware address	*/
+	unsigned char		ar_tip[4];		/* target IP address		*/
+};
+
+
+void decode_arp(const u_char *header_start) {
+  const struct my_arphdr *arp_header;
+  int i;
+
+  arp_header = (const struct my_arphdr *)header_start;
+  printf("((( Layer 3 ::: ARP Header )))\n");
+  printf("hardware address format: 0x%hx", ntohs(arp_header->ar_hrd));
+  printf("\tlength: %hhu\n", arp_header->ar_hln);
+  printf("protocol address format: 0x%hx", ntohs(arp_header->ar_pro));
+  printf("\tlength: %hhu\n", arp_header->ar_pln);
+  printf("ARP opcode: 0x%hx\n", ntohs(arp_header->ar_op));
+
+  printf("Sender MAC: ");
+  for(i = 0; i < ETH_ALEN; i++) {
+    if (i > 0) printf(":");
+    printf("%hhX", arp_header->ar_sha[i]);
+  }
+  printf("\tIP: ");
+  for(i = 0; i < 4; i++) {
+    if (i > 0) printf(".");
+    printf("%hhu", arp_header->ar_sip[i]);
+  }
+
+  
+  printf("\nTarget MAC: ");
+  for(i = 0; i < ETH_ALEN; i++) {
+    if (i > 0) printf(":");
+    printf("%hhX", arp_header->ar_tha[i]);
+  }
+  printf("\tIP: ");
+  for(i = 0; i < 4; i++) {
+    if (i > 0) printf(".");
+    printf("%hhu", arp_header->ar_tip[i]);
+  }
+
+  printf("\n");
+}
 
 void decode_ethernet(const u_char *header_start) {
   int i;
@@ -53,6 +108,7 @@ int main(int argc, char *argv[]) {
  printf("ARP packet... captured. Length: %d\n\n", recv_length);
 
  decode_ethernet(buffer);
+ decode_arp(buffer + ETH_HLEN);
     
  printf("\nssl_family: \t0x%hX\n", address->sll_family);
  printf("ssl_protocol\t0x%hX\n", ntohs(address->sll_protocol));
