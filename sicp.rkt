9f571a5e6e36688cdc171def7932908a811485b6
Pedro Toshiba <ppmauro@hotmail.com>
2017-11-02 23:36:27 -0300

    agrego 1.46 y termino la primera seccion

--- sicp.rkt
+++ sicp.rkt
@@ -910,3 +910,31 @@
 ;;;;;;;;;;;;;;;;;;;
 ;; Exercise 1.46 ;;
 ;;;;;;;;;;;;;;;;;;;
+
+(define (iterative-improve good-enough? improve)
+  (lambda (x)
+    (define (iter n)
+      (if (good-enough? n)
+	  n
+	  (iter (improve n))))
+    (iter x)))
+
+(define (fixed-point f first-guess)
+  (define tolerance 0.000001)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+
+  ((iterative-improve
+    (lambda (x)
+      (close-enough? x (f x)))
+    (lambda (x)
+      (f x)))
+   first-guess))
+
+(define (sqrt x)
+  ((iterative-improve
+    (lambda (y)
+      (< (abs (- (square y) x)) 0.0001))
+    (lambda (y)
+      (average y (/ x y))))
+   1.0))
