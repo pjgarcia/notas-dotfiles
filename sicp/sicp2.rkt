efe2616c5d7e22e1a8143fff512069ff95ec8406
Pedro Mauro <pedro@debian.bunsen.pedro>
2017-12-20 21:49:46 -0300

    agrego 2.66

--- sicp/sicp2.rkt
+++ sicp/sicp2.rkt
@@ -1468,3 +1468,15 @@
 	(list-2 (tree->list-2 tree-2)))
     (list->tree (intersection-set list-1 list-2))))
 	
+;;;;;;;;;;;;;;;;;;;
+;; Exercise 2.66 ;;
+;;;;;;;;;;;;;;;;;;;
+
+(define (lookup given-key set-of-records)
+  (cond ((null? set-of-records) #f)
+	((= given-key (key (entry set-of-records)))
+	 (entry set-of-records))
+	((< given-key (key (entry set-of-records)))
+	 (lookup given-key (left-branch set-of-records)))
+	(else
+	 (lookup given-key (right-branch set-of-records)))))
