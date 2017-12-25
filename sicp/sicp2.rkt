405b14093d40603b2693c0769d9839d19a2551bf
Pedro Mauro <pedro@debian.bunsen.pedro>
2017-12-25 17:57:41 -0300

    empiezo 2.67

--- sicp/sicp2.rkt
+++ sicp/sicp2.rkt
@@ -1529,6 +1529,27 @@
 	((= bit 1) (right-branch branch))
 	(else (error "bat bit: CHOOSE-BRANCH" bit))))
 
-	  
-	       
-	
+(define (huffman-adjoin-set x set)
+  (cond ((null? set) (list x))
+	((< (weight x) (weight (car set))) (cons x set))
+	(else (cons (car set) (huffman-adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+	(huffman-adjoin-set (make-leaf (car pair)  ;; symbol 
+				       (cadr pair)) ;; frequency
+			    (make-leaf-set (cdr pairs))))))
+
+;;;;;;;;;;;;;;;;;;;
+;; Exercise 2.67 ;;
+;;;;;;;;;;;;;;;;;;;
+
+(define sample-tree (make-code-tree (make-leaf 'A 4)
+				    (make-code-tree (make-leaf 'B 2)
+						    (make-code-tree (make-leaf 'D 1)
+								    (make-leaf 'C 1)))))
+
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+  
