bbd96e8eea8ad5fbc7c5c11be134e821904a3f6b
Pedro Toshiba <ppmauro@hotmail.com>
2018-01-09 00:12:19 -0300

    agrego todos los package-install

--- sicp/sicp2.rkt
+++ sicp/sicp2.rkt
@@ -1824,18 +1824,127 @@
 
   ;; interface to the rest of the system
   (define (tag x) (attach-tag 'rational x))
-  ;; (put-dispatch 'add '(rational rational)
-  ;; 		(lambda (x y) (tag (add-rat x y))))
+  (put-dispatch 'add '(rational rational)
+  		(lambda (x y) (tag (add-rat x y))))
   (put-dispatch 'sub '(rational rational)
   		(lambda (x y) (tag (add-rat x y))))
-  ;; (put-dispatch 'mul '(rational rational)
-  ;; 		(lambda (x y) (tag (mul-rat x y))))
-  ;; (put-dispatch 'div '(rational rational)
-  ;; 		(lambda (x y) (div (mul-rat x y))))
-  ;; (put-dispatch 'make 'rational
-  ;; 		(lambda (n d) (tag (make-rat n d))))
+  (put-dispatch 'mul '(rational rational)
+  		(lambda (x y) (tag (mul-rat x y))))
+  (put-dispatch 'div '(rational rational)
+  		(lambda (x y) (div (mul-rat x y))))
+  (put-dispatch 'make 'rational
+  		(lambda (n d) (tag (make-rat n d))))
   'done)
 
 (define (make-rational n d)
   ((get-dispatch 'make 'rational) n d))
 
+;; complex rectangular package
+(define (install-rectangular-package)
+  ;; internal procedures
+  (define (real-part z) (car z))
+  (define (imag-part z) (cdr z))
+  (define (magnitude z)
+    (sqrt (+ (square (real-part z))
+	     (square (imag-part z)))))
+  (define (angle z)
+    (atan (imag-part z) (real-part z)))
+  (define (make-from-real-imag x y)
+    (cons x y))
+  (define (make-from-mag-ang r a)
+    (cons (* r (cos a)) (* r (sin a))))
+  ;; interface to the rest of the system
+  (define (tag x)
+    (attach-tag 'rectangular x))
+  (put-dispatch 'real-part '(rectangular) real-part)
+  (put-dispatch 'imag-part '(rectangular) imag-part)
+  (put-dispatch 'magnitude '(rectangular) magnitude)
+  (put-dispatch 'angle '(rectangular) angle)
+  (put-dispatch 'make-from-real-imag 'rectangular
+		(lambda (x y)
+		  (tag (make-from-real-imag x y))))
+  (put-dispatch 'make-from-mag-ang 'rectangular
+		(lambda (r a)
+		  (tag (make-from-mag-ang r a))))
+  'done)
+
+;; complex polar package
+(define (install-polar-package)
+  ;; internal procedures
+  (define (magnitude z) (car z))
+  (define (angle z) (cdr z))
+  (define (make-from-mag-ang r a) (cons r a))
+  (define (real-part z)
+    (* (magnitude z) (cos (angle z))))
+  (define (imag-part z)
+    (* (magnitude z) (sin (angle z))))
+  (define (make-from-real-imag x y)
+    (cons (sqrt (+ (square x) (square y)))
+	  (atan y x)))
+  ;; interface to the rest of the system
+  (define (tag x) (attach-tag 'polar x))
+  (put-dispatch 'real-part '(polar) real-part)
+  (put-dispatch 'imag-part '(polar) imag-part)
+  (put-dispatch 'magnitude '(polar) magnitude)
+  (put-dispatch 'angle '(polar) magnitude)
+  (put-dispatch 'make-from-real-imag 'polar
+		(lambda (x y)
+		  (tag (make-from-real-imag x y))))
+  (put-dispatch 'make-from-mag-ang 'polar
+		(lambda (r a)
+		  (tag (make-from-mag-ang r a))))
+  'done)
+
+(define (install-complex-package)
+  ;; imported procedures from rectangular
+  ;; and polar packages
+  (define (make-from-real-imag x y)
+    ((get-dispatch 'make 'rectangular) x y))
+  (define (make-from-mag-ang r a)
+    ((get-dispatch 'make 'polar) r a))
+  ;; internal procedures
+  (define (add-complex z1 z2)
+    (make-from-real-imag (+ (real-part z1) (real-part z2))
+			 (+ (imag-part z1) (imag-part z2))))
+  (define (sub-complex z1 z2)
+    (make-from-real-imag (- (real-part z1) (real-part z2))
+			 (- (imag-part z1) (imag-part z2))))
+  (define (mul-complex z1 z2)
+    (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+		       (+ (angle z1) (angle z2))))
+  (define (div-complex z1 z2)
+    (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+		       (- (angle z1) (angle z2))))
+  ;; interface to the rest of the system
+  (define (tag z) (attach-tag 'complex z))
+  (put-dispatch 'add '(complex complex)
+		(lambda (z1 z2)
+		  (tag (add-complex z1 z2))))
+  (put-dispatch 'sub '(complex complex)
+		(lambda (z1 z2)
+		  (tag (sub-complex z1 z2))))
+  (put-dispatch 'mul '(complex complex)
+		(lambda (z1 z2)
+		  (tag (mul-complex z1 z2))))
+  (put-dispatch 'div '(complex complex)
+		(lambda (z1 z2)
+		  (tag (div-complex z1 z2))))
+  (put-dispatch 'make-from-real-imag 'complex
+		(lambda (x y)
+		  (tag (make-from-real-imag x y))))
+  (put-dispatch 'make-from-mag-ang 'complex
+		(lambda (r a)
+		  (tag (make-from-mag-ang r a))))
+  'done)
+
+(define (make-complex-from-real-imag x y)
+  ((get-dispatch 'make-from-real-imag 'complex) x y))
+
+(define (make-complex-from-mag-ang r a)
+  ((get-dispatch 'make-from-mag-ang 'complex) r a))
+
+  
+  
+			  
+		
+  
