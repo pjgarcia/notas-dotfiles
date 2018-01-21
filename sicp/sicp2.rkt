665d1ee13bb963091190793d298a482846705d7d
Pedro Toshiba <ppmauro@hotmail.com>
2018-01-21 03:12:06 -0300

    avanzo

--- sicp/sicp2.rkt
+++ sicp/sicp2.rkt
@@ -2481,6 +2481,8 @@
 
 (define (make-sparse-term order coeff)
   ((get-dispatch 'make-term 'sparse) order coeff))
+(define (the-empty-sparse-termlist)
+  ((get-dispatch 'the-empty-termlist 'sparse)))
 
 ;; (5 2 0 2 3 1)
 (define (install-dense-terms-package)
@@ -2492,12 +2494,12 @@
 	  (else
 	   (cons (coeff term) term-list))))
   (define (the-empty-termlist) '())
+  (define (make-term order coeff) (list order coeff))
   (define (first-term term-list)
     (make-term (- (length term-list) 1)
 	       (car term-list)))
   (define (rest-terms term-list) (cdr term-list))
   (define (empty-termlist? term-list) (null? term-list))
-  (define (make-term order coeff) (list order coeff))
   (define (order term) (car term))
   (define (coeff term) (cadr term))
 
@@ -2529,4 +2531,103 @@
 
 (define (adjoin-term term term-list)
   (apply-generic 'adjoin-term term term-list))
-;; rest of interface functions
+(define (first-term term-list)
+  (apply-generic 'first-term term-list))
+(define (rest-terms term-list)
+  (apply-generic 'rest-terms term-list))
+(define (empty-termlist? term-list)
+  (apply-generic 'empty-termlist? term-list))
+(define (order term)
+  (apply-generic 'order term))
+(define (coeff term)
+  (apply-generic 'coeff term))
+
+;; listas las interfaces de las term-list
+;; ahora queda hacer que el paquete de polys
+;; las use
+
+(define (install-polynomial-package2)
+  ;; internal procedures
+  ;; representation of poly
+  (define (make-poly variable term-list)
+    (cons variable term-list))
+  (define (variable p) (car p))
+  (define (term-list p) (cdr p))
+  (define (variable? x) (symbol? x))
+  (define (same-variable? v1 v2)
+    (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+  ;; representation of terms and term lists
+  (define (add-terms L1 L2)
+    (cond ((empty-termlist? L1) L2)
+	  ((empty-termlist? L2) L1)
+	  (else
+	   (let ((t1 (first-term L1))
+		 (t2 (first-term L2)))
+	     (cond ((> (order t1) (order t2))
+		    (adjoin-term t1 (add-terms (rest-terms L1) L2)))
+		   ((> (order t2) (order t1))
+		    (adjoin-term t2 (add-terms L1 (rest-terms L2))))
+		   (else
+		    (adjoin-term (make-sparse-term (order t1)
+						   (add (coeff t1) (coeff t2)))
+				 (add-terms (rest-terms L1)
+					    (rest-terms L2)))))))))
+  (define (mul-terms L1 L2)
+    (if (empty-termlist? L1)
+	(the-empty-sparse-termlist)
+	(add-terms (mul-term-by-all-terms (first-term L1) L2)
+		   (mul-terms (rest-terms L1) L2))))
+  (define (mul-term-by-all-terms t1 L)
+    (if (empty-termlist? L)
+	(the-empty-sparse-termlist)
+	(let ((t2 (first-term L)))
+	  (adjoin-term (make-sparse-term (+ (order t1) (order t2))
+					 (mul (coeff t1) (coeff t2)))
+		       (mul-term-by-all-terms t1 (rest-terms L))))))
+  (define (add-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+	(make-poly (variable p1)
+		   (add-terms (term-list p1)
+			      (term-list p2)))
+	(error "Polys not in same var -- ADD-POLY"
+	       (list p1 p2))))
+  (define (mul-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+	(make-poly (variable p1)
+		   (mul-terms (term-list p1)
+			      (term-list p2)))
+	(error "Polys not in same var -- MUL-POLY"
+	       (list p1 p2))))
+  ;; exercise 2.87
+  (define (=zero?-poly p)
+    (empty-termlist? (term-list p)))
+  ;; exercise 2.88
+  (define (negative-terms terms)
+    (if (empty-termlist? terms)
+	(the-empty-sparse-termlist)
+	(let ((first (first-term terms)))
+	  (adjoin-term (make-sparse-term (order first)
+					 (negative (coeff first)))
+		       (negative-terms (rest-terms terms))))))
+  (define (negative-poly p)
+    (make-poly
+     (variable p)
+     (negative-terms (term-list p))))
+  (define (sub-poly p1 p2)
+    (add-poly p1 (negative-poly p2)))
+
+  ;; interface to the rest of the system
+  (define (tag p) (attach-tag 'polynomial p))
+  (put-dispatch 'add '(polynomial polynomial)
+		(lambda (p1 p2) (tag (add-poly p1 p2))))
+  (put-dispatch 'mul '(polynomial polynomial)
+		(lambda (p1 p2) (tag (mul-poly p1 p2))))
+  (put-dispatch 'make 'polynomial
+		(lambda (var terms) (tag (make-poly var terms))))
+  (put-dispatch '=zero? '(polynomial) =zero?-poly)
+  (put-dispatch 'negative '(polynomial)
+		(lambda (p) (tag (negative-poly p))))
+  (put-dispatch 'sub '(polynomial polynomial)
+		(lambda (p1 p2) (tag (sub-poly p1 p2))))
+  'done)
