cd67e0fa2a869eaefef6980f96229ce3672ea74f
Pedro Toshiba <ppmauro@hotmail.com>
2018-03-12 22:40:33 -0300

    agrego ejercicio 3.21

--- sicp/sicp3-1.rkt
+++ sicp/sicp3-1.rkt
@@ -285,3 +285,62 @@
 
 (define should-be-endless2 (mcons 1 (mcons 2 (mcons 3 '()))))
 (set-mcdr! (mcdr (mcdr should-be-endless2)) should-be-endless2)
+
+;;;;;;;;;;;;;;;;;;;
+;; Exercise 3.21 ;;
+;;;;;;;;;;;;;;;;;;;
+
+(define (front-ptr queue)
+  (mcar queue))
+(define (rear-ptr queue)
+  (mcdr queue))
+(define (set-front-ptr! queue item)
+  (set-mcar! queue item))
+(define (set-rear-ptr! queue item)
+  (set-mcdr! queue item))
+
+;; QUEUE IMPLEMENTATION
+;; constructor
+(define (make-queue)
+  (mcons '() '()))
+;; accessors
+(define (empty-queue? queue)
+  (null? (front-ptr queue)))
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an empty queue" queue)
+      (mcar (front-ptr queue))))
+;; mutators
+(define (insert-queue! queue item)
+  (let ((new-pair (mcons item null)))
+    (cond ((empty-queue? queue)
+	   (set-front-ptr! queue new-pair)
+	   (set-rear-ptr! queue new-pair)
+	   queue)
+	  (else
+	   (set-mcdr! (rear-ptr queue) new-pair)
+	   (set-rear-ptr! queue new-pair)
+	   queue))))
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+	 (error "DELETE called with an empty queue" queue))
+	(else
+	 (set-front-ptr! queue (mcdr (front-ptr queue)))
+	 queue)))
+
+(define (print-queue queue)
+  (define (print-queue-contents items)
+    (cond ((null? items)
+	   (display ""))
+	  (else
+	   (display (mcar items))
+	   (display " ")
+	   (print-queue-contents (mcdr items)))))
+  (cond ((empty-queue? queue)
+	 (display "[ ]")
+	 (newline))
+	(else
+	 (display "[ ")
+	 (print-queue-contents (front-ptr queue))
+	 (display "]")
+	 (newline))))
