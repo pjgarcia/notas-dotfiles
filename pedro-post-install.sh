6d98677062fbcfd79983344a66d9982f5c8412f6
Pedro Mauro <pedro@debian-toshiba.piedras>
2018-03-28 19:54:22 -0400

    actualizo post-install

--- pedro-post-install.sh
+++ pedro-post-install.sh
@@ -3,3 +3,6 @@ sudo apt install emacs25 mupdf tmux cmusq
 sudo apt install net-tools dnsutils whois
 # duracion de la bateria
 sudo apt install acpi
+
+# racket: bajarlo de la pagina
+# geiser: seguir las instrucciones de instalacion de la pagina
