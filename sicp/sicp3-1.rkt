f943a48d6f43e76bb53226639ae395d2e9998ec1
Pedro Mauro <pedro@debian-toshiba.piedras>
2018-04-28 20:12:51 -0400

    agrego ejercicio 3.50

--- sicp/sicp3-1.rkt
+++ sicp/sicp3-1.rkt
@@ -1053,3 +1053,15 @@
 	(serializer2 (serializer1 exchange)))
      account1
      account2)))
+
+;;;;;;;;;;;;;;;;;;;
+;; Exercise 3.50 ;;
+;;;;;;;;;;;;;;;;;;;
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+	      (cons proc (map stream-cdr argstreams))))))
