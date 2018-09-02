335db2f828053c55986acce682ca1bb009d9d1db
Pedro M <pmauro@posteo.net>
2018-09-02 14:25:21 -0300

    agrego hasta 4.23

--- sicp/sicp4-1.scm
+++ sicp/sicp4-1.scm
@@ -555,8 +555,9 @@
   (tagged-list? p 'procedure))
 
 (define (procedure-parameters p) (cadr p))
-(define (procedure-body p)
-  (scan-out-defines (caddr p)))
+;; (define (procedure-body p)
+;;   (scan-out-defines (caddr p)))
+(define (procedure-body p) (caddr p))
 (define (procedure-environment p) (cadddr p))
 
 ;; operations on environments
@@ -653,6 +654,9 @@
 	(list 'square (lambda (x) (* x x)))
 	(list 'map my-map)
 	(list '+ +)
+	(list '= =)
+	(list '* *)
+	(list '- -)
 	(list 'first car)))
 
 (define (primitive-procedure-names)
@@ -767,8 +771,8 @@
 ;; Exercise 4.17 ;;
 ;;;;;;;;;;;;;;;;;;;
 
-in a correct program, both environment structures will work
-the same way.
+;; in a correct program, both environment structures will work
+;; the same way.
 
 ;;;;;;;;;;;;;;;;;;;
 ;; Exercise 4.18 ;;
@@ -846,3 +850,126 @@ the same way.
      (if (= n 0)
 	 #f
 	 (ev? ev? od? (- n 1))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 4.1.7 Separating Syntactic Analysis from Execution ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (eval exp env)
+  ((analyze exp) env))
+
+(define (analyze exp)
+  (cond ((self-evaluating? exp)
+	 (analyze-self-evaluating exp))
+	((quoted? exp) (analyze-quoted exp))
+	((variable? exp) (analyze-variable exp))
+	((assignment? exp) (analyze-assignment exp))
+	((definition? exp) (analyze-definition exp))
+	;; added by exercise 4.22
+	((let? exp) (analyze (let->combination1 exp)))
+	((if? exp) (analyze-if exp))
+	((lambda? exp) (analyze-lambda exp))
+	((begin? exp) (analyze-sequence (begin-actions exp)))
+	((cond? exp) (analyze (cond->if exp)))
+	((application? exp) (analyze-application exp))
+	(else
+	 (error "Unknown expression type -- ANALYZE" exp))))
+
+(define (analyze-self-evaluating exp)
+  (lambda (env) exp))
+
+(define (analyze-quoted exp)
+  (let ((qval (text-of-quotation exp)))
+    (lambda (env) qval)))
+
+(define (analyze-variable exp)
+  (lambda (env)
+    (lookup-variable-value exp env)))
+
+(define (analyze-assignment exp)
+  (let ((var (assignment-variable exp))
+	(valproc (analyze (assignment-value exp))))
+    (lambda (env)
+      (set-variable-value! var (valproc (env)) env)
+      'ok)))
+
+(define (analyze-definition exp)
+  (let ((var (definition-variable exp))
+	(valproc (analyze (definition-value exp))))
+    (lambda (env)
+      (define-variable! var (valproc env) env)
+      'ok)))
+
+(define (analyze-if exp)
+  (let ((pproc (analyze (if-predicate exp)))
+	(cproc (analyze (if-consequent exp)))
+	(aproc (analyze (if-alternative exp))))
+    (lambda (env)
+      (if (true? (pproc env))
+	  (cproc env)
+	  (aproc env)))))
+
+(define (analyze-lambda exp)
+  (let ((vars (lambda-parameters exp))
+	(bodyproc (analyze-sequence (lambda-body exp))))
+    (lambda (env)
+      (make-procedure vars bodyproc env))))
+
+(define (analyze-sequence exps)
+  (define (sequentially proc1 proc2)
+    (lambda (env) (proc1 env) (proc2 env)))
+  (define (loop first-proc rest-procs)
+    (if (null? rest-procs)
+	first-proc
+	(loop (sequentially first-proc (car rest-procs))
+	      (cdr rest-procs))))
+  (let ((procs (map analyze exps)))
+    (if (null? procs)
+	(error "Empty sequence -- ANALYZE"))
+    (loop (car procs) (cdr procs))))
+
+(define (analyze-application exp)
+  (let ((fproc (analyze (operator exp)))
+	(aprocs (map analyze (operands exp))))
+    (lambda (env)
+      (execute-application
+       (fproc env)
+       (map (lambda (aproc) (aproc env)) aprocs)))))
+
+(define (execute-application proc args)
+  (cond ((primitive-procedure? proc)
+	 (apply-primitive-procedure proc args))
+	((compound-procedure? proc)
+	 ((procedure-body proc)
+	  (extend-environment (procedure-parameters proc)
+			      args
+			      (procedure-environment proc))))
+	(else
+	 (error "Unknown procedure type -- EXECUTE-APPLICATION"
+		proc))))
+
+;;;;;;;;;;;;;;;;;;;
+;; Exercise 4.22 ;;
+;;;;;;;;;;;;;;;;;;;
+
+;; hecho en la definicion de "analyze" arriba
+
+;;;;;;;;;;;;;;;;;;;
+;; Exercise 4.23 ;;
+;;;;;;;;;;;;;;;;;;;
+
+;; for a 1 element sequence:
+;; + original version will directly evaluate the execution procedure
+;; + alyssa's version will execute a cond expression to evaluate
+;; if the sequence involves other procedures, and evaluate that
+;; first execution procedure
+
+;; for a 2 (or more) element sequence:
+;; + original version will call the wrapper "execution procedure"
+;; which will call the first execution procedure and after that, the second.
+;; + alyssa's version will analyze if the sequence has one or more
+;; elements, call the first, and then recursively call itself to, in the end, analyze the second item in the sequence.
+
+;;;;;;;;;;;;;;;;;;;
+;; Exercise 4.24 ;;
+;;;;;;;;;;;;;;;;;;;
