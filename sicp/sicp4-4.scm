bb480ac4c485e04de156e2b78733d76a9d527296
Pedro M <pmauro@posteo.net>
2018-10-09 00:51:02 -0300

    agrego hasta 4.58

--- sicp/sicp4-4.scm
+++ sicp/sicp4-4.scm
@@ -19,5 +19,30 @@
 ;;         (not (job ?boss (computer . ?supervisor-job)))
 ;;         (job ?boss ?job))
 
+;;;;;;;;;;;;;;;;;;;
+;; Exercise 4.57 ;;
+;;;;;;;;;;;;;;;;;;;
+
+(rule (can-replace ?person-1 ?person-2)
+      (and (and (job ?person-1 ?job)
+		(job ?person-2 ?job))
+	   (not (same ?person-1 ?person-2))))
+
+(can-replace ?person (Fect Cy D))
+
+(and (can-replace ?person-1 ?person-2)
+     (salary ?person-1 ?salary-1)
+     (salary ?person-2 ?salary-2)
+     (lisp-value > salary-2 salary-1))
+
+;;;;;;;;;;;;;;;;;;;
+;; Exercise 4.58 ;;
+;;;;;;;;;;;;;;;;;;;
+
+(rule (big-shot ?person ?division)
+      (and (job ?person ?division)
+	   (not (supervisor ?person ?supervisor)
+		(job ?supervisor ?division))))
 
 
+		
