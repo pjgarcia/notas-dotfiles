98137ff5278b01f8caab3b3543217fcaada47154
Pedro M <pmauro@posteo.net>
2018-12-29 01:16:15 -0300

    update

--- sicp/sicp5-5.scm
+++ sicp/sicp5-5.scm
@@ -100,3 +100,128 @@
 			     (reg val)
 			     (reg env))
 		    (assign ,target (const ok))))))))
+
+;; Compiling conditional expressions
+(define (compile-if exp target linkage)
+  (let ((t-branch (make-label 'true-branch))
+	(f-branch (make-label 'false-branch))
+	(after-if (make-label 'after-if)))
+    (let ((consequent-linkage
+	   (if (eq? linkage 'next) after-if linkage)))
+      (let ((p-code (compile (if-predicate exp) 'val 'next))
+	    (c-code
+	     (compile
+	      (if-consequent exp) target consequent-linkage))
+	    (a-code
+	     (compile (if-alternative exp) target linkage)))
+	(preserving '(env continue)
+		    p-code
+		    (append-instruction-sequences
+		     (make-instruction-sequence '(val) '()
+						`((test (op false?) (reg val))
+						  (branch (label ,f-branch))))
+		     (parallel-instruction-sequences
+		      (append-instruction-sequences t-branch c-code)
+		      (append-instruction-sequences f-branch a-code))
+		     after-if))))))
+
+(define label-counter 0)
+(define (new-label-number)
+  (set! label-counter (+ 1 label-counter))
+  label-counter)
+(define (make-label name)
+  (string->symbol
+   (string-append (symbol->string name)
+		  (number->string (new-label-number)))))
+
+;; Compiling sequences
+(define (compile-sequence seq target linkage)
+  (if (last-exp? seq)
+      (compile (first-exp seq) target linkage)
+      (preserving '(env continue)
+		  (compile (first-exp seq) target 'next)
+		  (compile-sequence (rest-exps seq) target linkage))))
+
+;; Compiling lambda expressions
+(define (compile-lambda exp target linkage)
+  (let ((proc-entry (make-label 'entry))
+	(after-lambda (make-label 'after-lambda)))
+    (let ((lambda-linkage
+	   (if (eq? linkage 'next) after-lambda linkage)))
+      (append-instruction-sequences
+       (tack-on-instruction-sequence
+	(end-with-linkage lambda-linkage
+			  (make-instruction-sequence '(env) (list target)
+						     `((assign ,target
+							       (op make-compiled-procedure)
+							       (label ,proc-entry)
+							       (reg env)))))
+	(compile-lambda-body exp proc-entry))
+       after-lambda))))
+
+(define (compile-lambda-body exp proc-entry)
+  (let ((formals (lambda-parameters exp)))
+    (append-instruction-sequences
+     (make-instruction-sequence
+      '(env proc argl) '(env)
+      `(,proc-entry
+	(assign env (op compiled-procedure-env) (reg proc))
+	(assign env
+		(op extend-environment)
+		(const ,formals)
+		(reg argl)
+		(reg env))))
+     (compile-sequence (lambda-body exp) 'val 'return))))
+
+;; Compiling Combinations
+(define (compile-application exp target linkage)
+  (let ((proc-code (compile (operator exp) 'proc 'next))
+	(operand-codes
+	 (map (lambda (operand) (compile operand 'val 'next))
+	      (operands exp))))
+    (preserving '(env continue)
+		proc-code
+		(preserving '(proc continue)
+			    (construct-arglist operand-codes)
+			    (compile-procedure-call target linkage)))))
+
+(define (construct-arglist operand-codes)
+  (let ((operand-codes (reverse operand-codes)))
+    (if (null? operand-codes)
+	(make-instruction-sequence '() '(argl)
+				   '((assign argl (const ()))))
+	(let ((code-to-get-last-arg
+	       (append-instruction-sequences
+		(car operand-codes)
+		(make-instruction-sequence '(val) '(argl)
+					   '((assign argl (op list) (reg val)))))))
+	  (if (null? (cdr operand-codes))
+	      code-to-get-last-arg
+	      (preserving '(env)
+			  code-to-get-last-arg
+			  (code-to-get-rest-args (cdr operand-codes))))))))
+
+(define (code-to-get-rest-args operand-codes)
+  (let ((code-for-next-arg
+	 (preserving '(argl)
+		     (car operand-codes)
+		     (make-instruction-sequence
+		      '(val argl) '(argl)
+		      '((assign argl (op cons) (reg val) (reg argl)))))))
+    (if (null? (cdr operand-codes))
+	code-for-next-arg
+	(preserving '(env)
+		    code-for-next-arg
+		    (code-to-get-rest-args (cdr operand-codes))))))
+		     
+
+	
+		     
+	
+
+(define (make-compiled-procedure entry env)
+  (list 'compiled-procedure entry env))
+(define (compiled-procedure? proc)
+  (tagged-list? proc 'compiled-procedure))
+(define (compiled-procedure-entry c-proc) (cadr c-proc))
+(define (compiled-procedure-env c-proc) (caddr c-proc))
