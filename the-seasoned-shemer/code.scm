3e0d10bc742db4c9ef9268be475cac5375f37b9c
Pedro Mauro <pedro.j.garcia@despegar.com>
2019-11-30 19:06:44 -0300

    update con capi 2

--- the-seasoned-shemer/code.scm
+++ the-seasoned-shemer/code.scm
@@ -1,3 +1,7 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 11. Welcome Back to the Show ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (define two-in-a-row-b?
   (lambda (preceding lat)
     (cond
@@ -29,7 +33,7 @@
 
 (define sum-of-prefixes
   (lambda (tup)
-    (sum-of-prefixes-b 0 tup)))++
+    (sum-of-prefixes-b 0 tup)))
 
 (define pick
   (lambda (n lat)
@@ -52,3 +56,106 @@
   (lambda (tup)
     (scramble-b tup (quote ()))))
     
+;;;;;;;;;;;;;;;;;;;;
+;; 12. Take Cover ;;
+;;;;;;;;;;;;;;;;;;;;
+
+(define multirember
+  (lambda (a lat)
+    (letrec
+	((mr (lambda (lat)
+	       (cond
+		((null? lat) (quote ()))
+		((eq? a (car lat))
+		 (mr (cdr lat)))
+		(else
+		 (cons (car lat)
+		       (mr (cdr lat))))))))
+      (mr lat))))
+
+(define union
+  (lambda (set1 set2)
+    (letrec
+	((M? (lambda (a lat)
+	       (letrec
+		   ((m? (lambda (lat)			  
+			  (cond
+			   ((null? lat) #f)
+			   ((eq? a (car lat)) #t)
+			   (else
+			    (m? (cdr lat)))))))
+		 (m? lat))))
+	 (U (lambda (set1)
+	      (cond
+	       ((null? set1) set2)
+	       ((M? (car set1) set2)
+		(U (cdr set1)))
+	       (else
+		(cons (car set1)
+		      (U (cdr set1))))))))
+      (U set1))))
+
+(define two-in-a-row-b?
+  (lambda (preceding lat)
+    (cond
+     ((null? lat) #f)
+     (else
+      (or (eq? preceding (car lat))
+	  (two-in-a-row-b?
+	   (car lat)
+	   (cdr lat)))))))
+    
+;; es two-in-a-row? pero protegiendo la definicion de two-in-a-row-b
+(define new-two-in-a-row?
+  (letrec
+      ((W (lambda (preceding lat)
+	    (cond
+	     ((null? lat) #f)
+	     (else
+	      (or (eq? preceding (car lat))
+		  (W
+		   (car lat)
+		   (cdr lat))))))))
+    (lambda (lat)
+      (cond
+       ((null? lat) #f)
+       (else
+	(W (car lat) (cdr lat))))))
+
+(define sum-of-prefixes-b
+  (lambda (sonssf tup)
+    (cond
+     ((null? tup) (quote ()))
+     (else
+      (cons (+ sonssf (car tup))
+	    (sum-of-prefixes-b
+	     (+ sonssf (car tup))
+	     (cdr tup)))))))
+
+;; also protected
+(define new-sum-of-prefixes
+  (letrec
+      ((S (lambda (sonssf tup)
+	    (cond
+	     ((null? tup) (quote ()))
+	     (else
+	      (cons (+ sonssf (car tup))
+		    (S (+ sonssf (car tup))
+		       (cdr tup))))))))
+    (lambda (tup)
+      (S 0 tup))))
+
+
+;; also protected
+(define scramble
+  (letrec
+      ((S (lambda (tup rev-pre)
+	    (cond
+	     ((null? tup) (quote ()))
+	     (else
+	      (cons (pick (car tup)
+			  (cons (car tup) rev-pre))
+		    (S (cdr tup)
+		       (cons (car tup) rev-pre))))))))
+    (lambda (tup)
+      (S tup (quote ())))))
