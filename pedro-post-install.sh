bf7da417ac177e6f98e7ea96a0b7cb225a690d38
Pedro Mauro <pedro.j.garcia@despegar.com>
2024-11-28 12:23:35 -0300

    elimino archivos viejos

--- pedro-post-install.sh
+++
@@ -1,8 +0,0 @@
-sudo apt install emacs26 mupdf tmux cmus
-# networking
-sudo apt install net-tools dnsutils whois
-# duracion de la bateria
-sudo apt install acpi
-
-# racket: bajarlo de la pagina
-# geiser: seguir las instrucciones de instalacion de la pagina
